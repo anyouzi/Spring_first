4dcaf76ef33f7571ac3d494bab359bfbcaa857ec
anyouzi <gunzenba2@gmail.com>
2023-05-15 13:14:34 +0800

    Initial commit

---
+++ src/main/java/com/example/spring_first/model/Student.java
@@ -0,0 +1,14 @@
+package com.example.spring_first.model;
+
+public class Student {
+    private String studentNo;//学号
+
+    private String loginPwd;//密码
+
+    private String studentName;//学生姓名
+
+
+    private String sex;//性别
+
+    private int gradeId;//年纪id
+}
